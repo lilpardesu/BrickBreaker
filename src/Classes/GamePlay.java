3360868c88ff74a0b9d65aa48360809927a03c3b
lilpardesu <89980151+lilpardesu@users.noreply.github.com>
2022-07-11 11:54:54 -0700

    Co-authored-by: EternalC0der <EternalC0der@users.noreply.github.com>

--- src/Classes/GamePlay.java
+++ src/Classes/GamePlay.java
@@ -14,7 +14,7 @@ import javax.swing.event.AncestorListener;
 import javax.swing.plaf.FontUIResource;
 import javax.swing.Timer;
 
-public class GamePlay extends JPanel implements KeyListener, AncestorListener, ActionListener{
+public class GamePlay extends JPanel implements KeyListener, AncestorListener, ActionListener {
     private int score = 0;
     private int totalbricks = 21;
     private Timer timer;
@@ -25,6 +25,8 @@ public class GamePlay extends JPanel implements KeyListener, AncestorListener, A
     private int BallPosY = 350;
     private int BalldirX = -2;
     private int BalldirY = -3;
+
+    private Rectangle ballRectangle;
 
     private MapGenerator map;
 
@@ -40,78 +42,77 @@ public class GamePlay extends JPanel implements KeyListener, AncestorListener, A
         timer.start();
     }
 
-    
     public void paint(Graphics g) {
 
         super.paint(g);
-        
-        //background
+
+        // background
         g.setColor(Color.black);
         g.fillRect(1, 1, 692, 592);
 
-        //map
-        map.draw((Graphics2D)g);
+        // map
+        map.draw((Graphics2D) g, this.ballRectangle);
 
-        //boarders
+        // boarders
         g.setColor(Color.yellow);
         g.fillRect(0, 0, 3, 592);
         g.fillRect(0, 0, 692, 3);
         g.fillRect(680, 0, 3, 592);
 
-        //paddle
+        // paddle
         g.setColor(Color.cyan);
         g.fillRect(PlayerPosX, 550, 100, 8);
 
-        //ball
+        // ball
         g.setColor(Color.yellow);
         g.fillOval(BallPosX, BallPosY, 20, 20);
 
-        //score
+        // score
         g.setColor(Color.white);
         g.setFont(new FontUIResource("impact", FontUIResource.BOLD, 25));
         g.drawString("SCORE: " + score, 80, 35);
 
         g.dispose();
     }
+
     @Override
     public void ancestorAdded(AncestorEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
+
     @Override
     public void ancestorRemoved(AncestorEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
+
     @Override
     public void ancestorMoved(AncestorEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
-        
-    
+
     @Override
     public void keyTyped(java.awt.event.KeyEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void keyPressed(java.awt.event.KeyEvent e) {
-        
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if(PlayerPosX >= 600) {
-                PlayerPosX = 600;
-            }
-            else {
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (PlayerPosX >= 580) {
+                PlayerPosX = 580;
+            } else {
                 MoveRight();
             }
         }
-        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if(PlayerPosX < 10) {
-                PlayerPosX = 10;
-            }
-            else {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (PlayerPosX < 10) {
+                PlayerPosX = 5;
+            } else {
                 MoveLeft();
                 System.out.println("keypress is working");
             }
@@ -130,54 +131,67 @@ public class GamePlay extends JPanel implements KeyListener, AncestorListener, A
     @Override
     public void keyReleased(java.awt.event.KeyEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // moves the ball
         System.out.println("game is running");
         BallPosX = BallPosX + BalldirX;
         BallPosY = BallPosY + BalldirY;
 
-        if (BallPosX < 0){
+        // keeps the ball in the jframe
+        if (BallPosX < 0) {
             BalldirX = -BalldirX;
         }
-        if (BallPosY < 0){
+        if (BallPosY < 0) {
             BalldirY = -BalldirY;
         }
-        if (BallPosX > 665){
+        if (BallPosX > 665) {
             BalldirX = -BalldirX;
         }
-        if (new Rectangle(BallPosX, BallPosY, 20, 20).intersects(new Rectangle(PlayerPosX,550,100,8))){
+
+        // touching the paddle
+        if (new Rectangle(BallPosX, BallPosY, 20, 20).intersects(new Rectangle(PlayerPosX, 550, 100, 8))) {
             BalldirY = -BalldirY;
         }
 
-        outerloop: for (int i = 0; i < map.map.length; i++){
-            for (int j = 0; j < map.map[0].length; j++){
-                if (map.map[i][j] > 0){
+        // draws rectangles around bricks
+        outerloop: for (int i = 0; i < map.map.length; i++) {
+            for (int j = 0; j < map.map[0].length; j++) {
+                if (map.map[i][j] > 0) {
                     int brickX = map.brickWidth * j + 80;
                     int brickY = map.brickHieght * i + 50;
 
                     Rectangle brickRectangle = new Rectangle(brickX, brickY, map.brickWidth, map.brickHieght);
-                    Rectangle ballRectangle = new Rectangle(BallPosX, BallPosY, 20, 20);
-                    
-                    if (ballRectangle.intersects(brickRectangle)){
+                    this.ballRectangle = new Rectangle(BallPosX, BallPosY, 20, 20);
+
+                    // checks if the ball touches any bricks
+                    if (this.ballRectangle.intersects(brickRectangle)) {
                         map.setBrickValue(0, i, j);
 
                         totalbricks = totalbricks - 1;
                         score = score + 1;
 
-                        if(BallPosX + 19 <= brickRectangle.x || BallPosX + 1 >= brickRectangle.x + map.brickWidth){
+                        // Check if the ball touches the top or bottom of the brick
+                        if (BallPosY + 21 >= brickRectangle.getY() && BallPosY - 1 <= brickRectangle.getY() + map.brickHieght) {
+                            BalldirY = -BalldirY;
+                        } else {
                             BalldirX = -BalldirX;
                         }
-                        else{
+
+                        // Check if the ball touches the left or right of the brick
+                        if (BallPosX + 21 >= brickRectangle.getX() && BallPosX + 21 <= brickRectangle.getX() + map.brickWidth) {
+                            BalldirX = -BalldirX;
+                        } else {
                             BalldirY = -BalldirY;
                         }
 
                         break outerloop;
                     }
                 }
-                
+
             }
         }
         repaint();
--- src/Classes/MapGenerator.java
+++ src/Classes/MapGenerator.java
@@ -10,20 +10,44 @@ public class MapGenerator {
     public MapGenerator(int row, int col) {
         map = new int[row][col];
         for (int i = 0; i < map.length; i++) {
-            for(int j = 0; j < map[0].length; j++) {
+            for (int j = 0; j < map[0].length; j++) {
                 map[i][j] = 1;
             }
         }
 
-        brickWidth = 540/col;
-        brickHieght = 150/row;
+        brickWidth = 540 / col;
+        brickHieght = 150 / row;
     }
 
-    public void draw(Graphics2D g) {
+    private void drawBallBorders(Graphics2D g, Rectangle ballRectangle) {
+        // float[] dashingPattern1 = { 2f, 2f };
+        // Stroke stroke2 = new BasicStroke(2f, BasicStroke.CAP_BUTT,
+        //         BasicStroke.JOIN_MITER, 1.0f, dashingPattern1, 2.0f);
+        // g.setStroke(stroke2);
+        g.setStroke(new BasicStroke(1));
+        g.setColor(Color.RED);
+        g.drawRect(ballRectangle.x - 1, ballRectangle.y - 1, 21, 21);
+    }
+
+    private void drawBrickBorders(Graphics2D g, Rectangle brickRectangle) {
+        // float[] dashingPattern1 = { 2f, 2f };
+        // Stroke stroke2 = new BasicStroke(2f, BasicStroke.CAP_BUTT,
+        //         BasicStroke.JOIN_MITER, 1.0f, dashingPattern1, 2.0f);
+        // g.setStroke(stroke2);
+        g.setStroke(new BasicStroke(1));
+        g.setColor(Color.RED);
+        g.drawRect(brickRectangle.x - 1, brickRectangle.y - 1, brickRectangle.width + 1, brickRectangle.height + 1);
+    }
+
+    /**
+     * @param g
+     * @param ballRectangle
+     */
+    public void draw(Graphics2D g, Rectangle ballRectangle) {
         for (int i = 0; i < map.length; i++) {
-            for(int j = 0; j < map[0].length; j++) {
-                map[i][j] = 1;
-                if (map[i][j] > 0) {
+            for (int j = 0; j < map[0].length; j++) {
+                // map[i][j] = 1;
+                if (map[i][j] == 1) {
                     g.setColor(Color.white);
                     g.fillRect(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght);
 
@@ -31,13 +55,15 @@ public class MapGenerator {
                     g.setColor(Color.black);
                     g.drawRect(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght);
 
-                }        
+                    // drawBallBorders(g, ballRectangle);
+                    // drawBrickBorders(g, new Rectangle(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght));
+                }
             }
         }
     }
 
     public void setBrickValue(int value, int i, int j) {
         map[i][j] = value;
-        
+
     }
 }
