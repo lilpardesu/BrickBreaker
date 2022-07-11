320ac87c7139522d02b35098a3cf9d9976c8a667
lilpardesu <89980151+lilpardesu@users.noreply.github.com>
2022-07-11 11:57:31 -0700

    meow

--- src/Classes/MapGenerator.java
+++ src/Classes/MapGenerator.java
@@ -19,26 +19,6 @@ public class MapGenerator {
         brickHieght = 150 / row;
     }
 
-    private void drawBallBorders(Graphics2D g, Rectangle ballRectangle) {
-        // float[] dashingPattern1 = { 2f, 2f };
-        // Stroke stroke2 = new BasicStroke(2f, BasicStroke.CAP_BUTT,
-        //         BasicStroke.JOIN_MITER, 1.0f, dashingPattern1, 2.0f);
-        // g.setStroke(stroke2);
-        g.setStroke(new BasicStroke(1));
-        g.setColor(Color.RED);
-        g.drawRect(ballRectangle.x - 1, ballRectangle.y - 1, 21, 21);
-    }
-
-    private void drawBrickBorders(Graphics2D g, Rectangle brickRectangle) {
-        // float[] dashingPattern1 = { 2f, 2f };
-        // Stroke stroke2 = new BasicStroke(2f, BasicStroke.CAP_BUTT,
-        //         BasicStroke.JOIN_MITER, 1.0f, dashingPattern1, 2.0f);
-        // g.setStroke(stroke2);
-        g.setStroke(new BasicStroke(1));
-        g.setColor(Color.RED);
-        g.drawRect(brickRectangle.x - 1, brickRectangle.y - 1, brickRectangle.width + 1, brickRectangle.height + 1);
-    }
-
     /**
      * @param g
      * @param ballRectangle
@@ -46,7 +26,6 @@ public class MapGenerator {
     public void draw(Graphics2D g, Rectangle ballRectangle) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
-                // map[i][j] = 1;
                 if (map[i][j] == 1) {
                     g.setColor(Color.white);
                     g.fillRect(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght);
@@ -54,9 +33,6 @@ public class MapGenerator {
                     g.setStroke(new BasicStroke(3));
                     g.setColor(Color.black);
                     g.drawRect(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght);
-
-                    // drawBallBorders(g, ballRectangle);
-                    // drawBrickBorders(g, new Rectangle(j * brickWidth + 80, i * brickHieght + 50, brickWidth, brickHieght));
                 }
             }
         }
